a23bfb9c5ac977ae80a3dbdef9d6360419ff9ae3
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-04-22 18:18:38 +0200

    Set simple server to listen on 3000

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+ *  Primary file for the API
+ *
+ */
+
+// Dependencies
+
+const http = require('http');
+
+// The server should responde to all requests with a string
+const server = http.createServer((req, res) => {
+    res.end('Hello world\n');
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => console.log('The server is listenning on port 300 now'));
