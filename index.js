f7fea9efcafe232ada04acd53df98c26d87b83a4
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-04-22 19:17:42 +0200

    Parse request path

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
 // Dependencies
 
 const http = require('http');
+const url = require('url');
 
 // The server should responde to all requests with a string
 const server = http.createServer((req, res) => {
+
+    // Get the URL and parse it
+    // urlParse(url, parseQueryString) parseQueryString=true is using querystring core module
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl);
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');// remove slash on start and end
+
+    // Send the response
     res.end('Hello world\n');
+
+    // Log the request path
+    console.log(`Reuest received on path: ${trimmedPath}`);
+
 });
 
 // Start the server, and have it listen on port 3000
