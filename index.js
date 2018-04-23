9d830aa4d33dfaffddfc4200c38d1524da0601e1
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-04-23 06:10:50 +0200

    Routing requests

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     // Get the URL and parse it
     // urlParse(url, parseQueryString) parseQueryString=true is using querystring core module
     const parsedUrl = url.parse(req.url, true);
-    console.log(parsedUrl.query);
+
     // Get the path
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');// remove slash on start and end
@@ -31,22 +31,67 @@ const server = http.createServer((req, res) => {
 
     // Get the payload, if any
     const decoder = new StringDecoder('utf-8');
-    let buffer = '';
+    let payload = '';
     req.on('data', (data) => {// data event will not be fired if ther is no payload
-        buffer += decoder.write(data);
+        payload += decoder.write(data);
     });
 
     req.on('end', () => { // end event will always be called
-        buffer += decoder.end();
+        payload += decoder.end();
 
-        // Send the response
-        res.end('Hello world\n');
+        // Choose the handler this request should go to. If one is not found then choose the not found handler
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // Log the request path
-        console.log(`Request received with payload: ${buffer}`);
+        // Construct the data object to send to the handler
+        const data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload
+        };
+
+        // Route hte request to the handler specified in the router
+        chosenHandler(data, (statusCode, responsePayload) => {
+            // Use the status code called back by the handler, or default to the 200
+            statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
+
+            // Use the response payload called back by the router, or default to an empty object
+            responsePayload = typeof (responsePayload) === 'object' ? responsePayload : {};
+
+            // Convert the response Payload to a string
+            const payloadString = JSON.stringify(responsePayload);
+
+            // Return the response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the response
+            console.log(`Returning this response: ${statusCode} ${payloadString}`);
+        });
+
     });
 
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => console.log('The server is listenning on port 300 now'));
+
+// Define handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+// Callback a http status code, and a payload object
+    callback(406, {'name': 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+// Define a request router
+const router = {
+    'sample': handlers.sample
+};
